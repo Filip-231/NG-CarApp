e21df44a1258bacbd688567115db222aa23ea532
Filip-231 <filip.szmid@gmail.com>
2022-07-02 16:18:21 +0200

    deploy action

--- app/myapi/settings.py
+++ app/myapi/settings.py
@@ -32,9 +32,9 @@ DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
-
+# if ALLOWED_HOSTS_ENV:
+#     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
+ALLOWED_HOSTS.append(ALLOWED_HOSTS_ENV)
 
 # Application definition
 
